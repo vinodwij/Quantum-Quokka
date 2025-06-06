75f5d4cfd5601f57296058d5200499cabc833747
Vinod Wijetilleke <104864052+vinodwij@users.noreply.github.com>
2025-06-06 10:43:58 +0530

    CSS FIX

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,10 @@
-# utils.py
 import streamlit as st
+import os
 
 def load_css(file_name="style.css"):
-    """Load custom CSS from a file."""
-    with open(file_name) as f:
+    """Load custom CSS from a file located in the project root."""
+    css_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), file_name)
+    with open(css_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 def load_css_once(file_name="style.css"):
